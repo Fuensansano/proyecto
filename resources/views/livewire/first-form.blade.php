7924129db4f760e80cf3f885c496564420080d64
FuenSansano <fuensanosano@gmail.com>
2023-04-24 20:00:24 +0200

    casi terminada la vista del formulario de extraescolares a expensas del botón

--- resources/views/livewire/first-form.blade.php
+++ resources/views/livewire/first-form.blade.php
@@ -1,21 +1,139 @@
 <x-main>
     @livewire('navbar')
     <div class="container">
-
-        <div class="card">
-
-            <div class="card-body">
-                <div class="row row-cols-2">
-                    <div class="col">
-                        <label for="">label1</label>
+        <form action="save" method="post">
+        @csrf
+            <div class="card mt-5">
+                <h1 class="card-header text-primary">INFORMACIÓN SOBRE ACTIVIDAD EXTRAESCOLAR</h1>
+                <div class="card-body">
+                    {{-- primera fila --}}
+                    <div class="row row-cols-2">
+                        <div class="col">
+                            <label for="activity_name">Denominación de la actividad</label>
+                        </div>
+                        <div class="col">
+                            <label for="activity_place">Lugar de la actividad</label>
+                        </div>
+                        <div class="col">
+                            <input type="text" class="form-control mb-4" id="activity_name" name="activity_name"/>
+                        </div>
+                        <div class="col">
+                            <input type="text" class="form-control mb-4" id="activity_place" name="activity_place"/>
+                        </div>
                     </div>
-                    <div class="col">label2</div>
-                    <div class="col">
-                        <input type="text" class="form-control" />
+                    {{-- segunda fila --}}
+                    <div class="row row-cols-2">
+                        <div class="col">
+                            <label for="activity_module">Asignatura/modulo</label>
+                        </div>
+                        <div class="col">
+                            <label for="activity_departament">Departamento</label>
+                        </div>
+                        <div class="col">
+                            <input type="text" class="form-control mb-4" id="activity_module" name="activity_module"/>
+                        </div>
+                        <div class="col">
+                            <input type="text" class="form-control mb-4" id="activity_departament" name="activity_departament" />
+                        </div>
                     </div>
-                    <div class="col">Column</div>
+                    {{-- tercera fila --}}
+                    <div class="row row-cols-2">
+                        <div class="col">
+                            <label for="teachers">Profesores acompañantes</label>
+                        </div>
+                        <div class="col">
+                            <label for="student_groups">Grupos de alumnos que realizarán la actividad</label>
+                        </div>
+                        <div class="col">
+                            <textarea type="text" class="form-control mb-4" id="teachers" name="teachers"></textarea>
+                        </div>
+                        <div class="col">
+                            <textarea type="text" class="form-control mb-4" id="student_groups" name="student_groups"></textarea>
+                        </div>
+                    </div>
+
+                    {{-- cuarta fila --}}
+                    <div class="row row-cols-2">
+                        <div class="col">
+                            <label for="date">Fecha</label>
+                        </div>
+                        <div class="col">
+                            <label for="departure_time">Hora de salida</label>
+                        </div>
+                        <div class="col">
+                            <input type="date" class="form-control mb-4" id="date" name="date"></input>
+                        </div>
+                        <div class="col">
+                            <input type="time" class="form-control mb-4" id="departure_time" name="departure_time"></input>
+                        </div>
+
+                    </div>
+
+                    {{-- quinta fila --}}
+                    <div class="row row-cols-2">
+                        <div class="col">
+                            <label for="arrive_time">Hora prevista de llegada</label>
+                        </div>
+                        <div class="col">
+                            <label for="activity_price">Precio de la actividad</label>
+                        </div>
+                        <div class="col">
+                            <input type="time" class="form-control mb-4" id="departure_time" name="departure_time"></input>
+                        </div>
+                        <div class="col">
+                            <input type="text" class="form-control mb-4" id="activity_price" name="activity_price"></input>
+                        </div>
+                    </div>
+
+                    {{-- sexta fila --}}
+                    <div class="row row-cols-2">
+                        <div class="col">
+                            <label for="transport">Medio de transporte</label>
+                        </div>
+                        <div class="col">
+                            <label for="activity_responsible_teacher">Profesor responsable</label>
+                        </div>
+
+                        <div class="col">
+                            <input type="text" class="form-control mb-4" id="transport" name="transport"></input>
+                        </div>
+                        <div class="col">
+                            <input type="text" class="form-control mb-4" id="activity_responsible_teacher" name="activity_responsible_teacher" />
+                        </div>
+                    </div>
+
+                    {{-- séptima fila --}}
+                    <div class="row row-cols-1">
+                        <div class="col">
+                            <label for="observations">Observaciones</label>
+                        </div>
+                        <div class="col">
+                            <textarea type="time" class="form-control mb-4" id="observations" name="observations"></textarea>
+                        </div>
+                    </div>
                 </div>
+
+                <button type="button" class="btn btn-primary">Primary</button>
+
             </div>
-        </div>
+        </form>
     </div>
 </x-main>
+
+<style>
+    .card-header {
+        height: 80px;
+        border-style: none;
+    }
+
+    .container {
+        max-width: 90%;
+    }
+
+    h1 {
+        align-self: center;
+        font-size: 30px;
+        margin-top: 10px;
+    }
+
+</style>
