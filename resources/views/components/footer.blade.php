82e3cf9f90f42294ab1dd73f904fc2889c3e7840
tomasrl18 <tomaslopezraigal@gmail.com>
2023-04-28 19:12:33 +0200

    Header y Footer sacados a componentes

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,26 @@
+<footer>
+    <table>
+        <tr>
+            <td>
+                <ul>
+                    <li>
+                        <span>
+                            C.I.F.: Q3068165D
+                        </span>
+                    </li>
+                    <li>
+                        <a href="https://www.iescierva.net/">www.iescierva.net/</a>
+                    </li>
+                    <li>
+                        <a href="30010978@murciaeduca.es">30010978@murciaeduca.es</a>
+                    </li>
+                    <li>
+                        <span>
+                            Código Centro 30010978
+                        </span>
+                    </li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+</footer>
--- resources/views/pdfs/autorizacionFamiliarPDF.blade.php
+++ resources/views/pdfs/autorizacionFamiliarPDF.blade.php
@@ -1,10 +1,54 @@
+<x-header />
+
 <p>{{ $data['activity'] }}</p>
 <p>{{ $data['organizer'] }}</p>
 <p>{{ $data['execution_date'] }}</p>
 <p>{{ $data['departure_time'] }}</p>
 <p>{{ $data['goals'] }}</p>
-<p>{{ $data['deadline'] }}</p>
+<p style="page-break-before: always;">{{ $data['deadline'] }}</p>
 <p>{{ $data['parents'] }}</p>
 <p>{{ $data['student'] }}</p>
 <p>{{ $data['course'] }}</p>
 <p>{{ $data['authorization'] }}</p>
+
+<x-footer />
+
+<style>
+    header {
+        margin: 0
+        position: fixed;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+    }
+
+    footer {
+        position: fixed;
+        left: 0px;
+        bottom: -50px;
+        right: 0px;
+        height: 40px;
+        border-top: 1px solid #383232;
+    }
+
+    footer .page:after {
+        content: counter(page);
+    }
+
+    footer table {
+        width: 100%;
+    }
+
+    footer td {
+        text-align: center;
+        font-size: 10px;
+    }
+
+    footer span {
+        color: blue;
+    }
+
+    ul li {
+        display: inline;
+    }
+</style>
