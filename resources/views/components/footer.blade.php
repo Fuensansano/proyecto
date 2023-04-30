58c5bf464ddbb70e52cb2b233d667e5e637f3cb0
tomasrl18 <tomaslopezraigal@gmail.com>
2023-04-30 13:04:39 +0200

    Footer mejorado con sus circulos

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,33 @@
 <div id="footer">
-    <p class="textFooter">Texto del footer</p>
+    <ul class="footer-ul">
+        <li class="footer-li">
+            C.I.F.: Q3068165D
+        </li>
+        
+        <li class="footer-li">
+            <div id="circle"></div>
+        </li>
+        
+        <li class="footer-li">
+            <a href="www.iescierva.net">www.iescierva.net</a>
+        </li>
+
+        <li class="footer-li">
+            <div id="circle"></div>
+        </li>
+
+        <li class="footer-li">
+            <a href="30010978@murciaeduca.es">30010978@murciaeduca.es</a>
+        </li>
+
+        <li class="footer-li">
+            <div id="circle"></div>
+        </li>
+
+        <li class="footer-li">
+            Código Centro 30010978
+        </li>
+    </ul>
 </div>
 
 <style>
@@ -7,12 +35,29 @@
         position: fixed;
         bottom: -5%;
         left: 0;
-        border-top: 1px solid #383232;
+        border-top: 1px solid black;
         width: 100%;
     }
 
-    .textFooter {
-        text-align: center;
+    .footer-li {
         width: 100%;
+        color: blue;
+        font-size: 12px;
+    }
+
+    .footer-ul {
+        text-align: center;
+    }
+
+    .footer-ul li {
+        display: inline;
+    }
+
+    #circle {
+        display: inline-block;
+        height: 6px;
+        width: 6px;
+        background: blue;
+        border-radius: 50%;
     }
 </style>
