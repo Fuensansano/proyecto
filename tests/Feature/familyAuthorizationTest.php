a82bdbeacee57a618061d1bc4da66bac35de1861
tomasrl18 <tomaslopezraigal@gmail.com>
2023-04-30 11:23:42 +0200

    Añadido test DNI. Cambiado rutas tests

--- tests/Feature/familyAuthorizationTest.php
+++ tests/Feature/familyAuthorizationTest.php
@@ -10,70 +10,70 @@ class AutorizacionFamiliarTest extends TestCase
 {
     /** @test */
     function the_activity_field_exists_in_the_autorizacion_familiar_form_page() {
-        $this->get('/autorizacionFamiliar')
+        $this->get(route('authFamForm'))
             ->assertStatus(200)
             ->assertSee('ACTIVIDAD PROGRAMADA');
     }
 
     /** @test */
     function the_organizer_field_exists_in_the_autorizacion_familiar_form_page() {
-        $this->get('/autorizacionFamiliar')
+        $this->get(route('authFamForm'))
             ->assertStatus(200)
             ->assertSee('ORGANIZADOR/A');
     }
 
     /** @test */
     function the_execution_date_field_exists_in_the_autorizacion_familiar_form_page() {
-        $this->get('/autorizacionFamiliar')
+        $this->get(route('authFamForm'))
             ->assertStatus(200)
             ->assertSee('FECHA REALIZACIÓN');
     }
 
     /** @test */
     function the_departure_time_field_exists_in_the_autorizacion_familiar_form_page() {
-        $this->get('/autorizacionFamiliar')
+        $this->get(route('authFamForm'))
             ->assertStatus(200)
             ->assertSee('HORA SALIDA');
     }
 
     /** @test */
     function the_goals_field_exists_in_the_autorizacion_familiar_form_page() {
-        $this->get('/autorizacionFamiliar')
+        $this->get(route('authFamForm'))
             ->assertStatus(200)
             ->assertSee('OBJETIVOS Y CONTENIDOS');
     }
 
     /** @test */
     function the_deadline_field_exists_in_the_autorizacion_familiar_form_page() {
-        $this->get('/autorizacionFamiliar')
+        $this->get(route('authFamForm'))
             ->assertStatus(200)
             ->assertSee('FECHA ENTREGA');
     }
 
     /** @test */
     function the_parents_field_exists_in_the_autorizacion_familiar_form_page() {
-        $this->get('/autorizacionFamiliar')
+        $this->get(route('authFamForm'))
             ->assertStatus(200)
             ->assertSee('PADRE/MADRE/TUTOR');
     }
 
     /** @test */
     function the_student_field_exists_in_the_autorizacion_familiar_form_page() {
-        $this->get('/autorizacionFamiliar')
+        $this->get(route('authFamForm'))
             ->assertStatus(200)
             ->assertSee('ALUMNO');
     }
 
     /** @test */
     function the_course_field_exists_in_the_autorizacion_familiar_form_page() {
-        $this->get('/autorizacionFamiliar')
+        $this->get(route('authFamForm'))
             ->assertStatus(200)
             ->assertSee('CURSO DEL ALUMNO');
     }
 
     /** @test */
     function the_authorization_field_exists_in_the_autorizacion_familiar_form_page() {
-        $this->get('/autorizacionFamiliar')
+        $this->get(route('authFamForm'))
             ->assertStatus(200)
             ->assertSee('Tiene mi autorización para participar en la actividad programada y autorizo a la toma y difusión de imágenes de este día en la página web y/o RRSS del centro.')
             ->assertSee('No va a participar en la actividad programada.');
@@ -83,7 +83,7 @@ class AutorizacionFamiliarTest extends TestCase
     function the_activity_is_required() {
         /* $this->withExceptionHandling(); */
 
-        $this->from('/autorizacionFamiliar')
+        $this->from(route('authFamForm'))
             ->post(route('generarPDF'), [
                 'activity' => null,
             ])
@@ -95,7 +95,7 @@ class AutorizacionFamiliarTest extends TestCase
     function the_organizer_is_required() {
         /* $this->withExceptionHandling(); */
 
-        $this->from('/autorizacionFamiliar')
+        $this->from(route('authFamForm'))
             ->post(route('generarPDF'), [
                 'organizer' => null,
             ])
@@ -107,7 +107,7 @@ class AutorizacionFamiliarTest extends TestCase
     function the_execution_date_is_required() {
         /* $this->withExceptionHandling(); */
 
-        $this->from('/autorizacionFamiliar')
+        $this->from(route('authFamForm'))
             ->post(route('generarPDF'), [
                 'execution_date' => null,
             ])
@@ -119,7 +119,7 @@ class AutorizacionFamiliarTest extends TestCase
     function the_departure_time_is_required() {
         /* $this->withExceptionHandling(); */
 
-        $this->from('/autorizacionFamiliar')
+        $this->from(route('authFamForm'))
             ->post(route('generarPDF'), [
                 'departure_time' => null,
             ])
@@ -131,7 +131,7 @@ class AutorizacionFamiliarTest extends TestCase
     function the_goals_is_required() {
         /* $this->withExceptionHandling(); */
 
-        $this->from('/autorizacionFamiliar')
+        $this->from(route('authFamForm'))
             ->post(route('generarPDF'), [
                 'goals' => null,
             ])
@@ -143,7 +143,7 @@ class AutorizacionFamiliarTest extends TestCase
     function the_deadline_is_required() {
         /* $this->withExceptionHandling(); */
 
-        $this->from('/autorizacionFamiliar')
+        $this->from(route('authFamForm'))
             ->post(route('generarPDF'), [
                 'deadline' => null,
             ])
@@ -155,7 +155,7 @@ class AutorizacionFamiliarTest extends TestCase
     function the_parents_is_required() {
         /* $this->withExceptionHandling(); */
 
-        $this->from('/autorizacionFamiliar')
+        $this->from(route('authFamForm'))
             ->post(route('generarPDF'), [
                 'parents' => null,
             ])
@@ -167,7 +167,7 @@ class AutorizacionFamiliarTest extends TestCase
     function the_student_is_required() {
         /* $this->withExceptionHandling(); */
 
-        $this->from('/autorizacionFamiliar')
+        $this->from(route('authFamForm'))
             ->post(route('generarPDF'), [
                 'student' => null,
             ])
@@ -179,7 +179,7 @@ class AutorizacionFamiliarTest extends TestCase
     function the_course_is_required() {
         /* $this->withExceptionHandling(); */
 
-        $this->from('/autorizacionFamiliar')
+        $this->from(route('authFamForm'))
             ->post(route('generarPDF'), [
                 'course' => null,
             ])
@@ -191,11 +191,23 @@ class AutorizacionFamiliarTest extends TestCase
     function the_authorization_is_required() {
         /* $this->withExceptionHandling(); */
 
-        $this->from('/autorizacionFamiliar')
+        $this->from(route('authFamForm'))
             ->post(route('generarPDF'), [
                 'authorization' => null,
             ])
             ->assertSessionHasErrors('authorization')
+            ->assertRedirect(route('authFamForm'));
+    }
+
+    /** @test */
+    function the_dni_is_required() {
+        /* $this->withExceptionHandling(); */
+
+        $this->from(route('authFamForm'))
+            ->post(route('generarPDF'), [
+                'dni' => null,
+            ])
+            ->assertSessionHasErrors('dni')
             ->assertRedirect(route('authFamForm'));
     }
 }
