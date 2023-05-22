905801247aa32c1c2dfc533ff417740e4a5eb774
tomasrl18 <tomaslopezraigal@gmail.com>
2023-05-22 20:47:21 +0200

    Imagen del navbar ahora lleva a /

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-primary">
     <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <a class="navbar-brand" href="https://iescierva.net" target="_blank">
+            <a class="navbar-brand" href="/">
                 <img src="/images/ies-ingeniero-de-la-cierva-logo.webp" alt="Bootstrap" width="150" height="24">
             </a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
