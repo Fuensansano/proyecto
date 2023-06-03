004d81a69b493da19338a281a037c2830cddca1c
FuenSansano <fuensanosano@gmail.com>
2023-06-03 15:26:45 +0200

    REFACTOR: implement testHelper and made test more clear in FamilyAuthorizationTest class

--- tests/Feature/FamilyAuthorizationTest.php
+++ tests/Feature/FamilyAuthorizationTest.php
@@ -2,12 +2,27 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Tests\TestHelpers;
 
 class FamilyAuthorizationTest extends TestCase
 {
+    use TestHelpers;
+
+    protected array $defaultData = [
+        'activity' => 'a random activity',
+        'organizer' => 'a random organizer',
+        'execution date' => 'a random execution date',
+        'departure time' => 'a random departure time',
+        'goals' => 'a random goals',
+        'deadline' => 'a random deadline',
+        'parents' => 'a random parents',
+        'student' => 'a random student',
+        'course' => 'a random course',
+        'authorization' => 'a random authorization',
+        'dni' => 'a random dni',
+    ];
+
     /** @test */
     function the_activity_field_exists_in_the_autorizacion_familiar_form_page() {
         $this->get('/family-authorization')
@@ -89,99 +104,99 @@ class FamilyAuthorizationTest extends TestCase
     /** @test */
     function the_activity_is_required() {
         $this->from('/family-authorization')
-            ->post('/family-authorization', [
+            ->post('/family-authorization', $this->getValidData([
                 'activity' => null,
-            ])
+            ]))
             ->assertSessionHasErrors(['activity' => 'La actividad es obligatoria']);
     }
 
     /** @test */
     function the_organizer_is_required() {
         $this->from('/family-authorization')
-            ->post('/family-authorization', [
+            ->post('/family-authorization', $this->getValidData([
                 'organizer' => null,
-            ])
+            ]))
             ->assertSessionHasErrors(['organizer' => 'El nombre del organizador es obligatorio']);
     }
 
     /** @test */
     function the_execution_date_is_required() {
         $this->from('/family-authorization')
-            ->post('/family-authorization', [
+            ->post('/family-authorization', $this->getValidData([
                 'execution_date' => null,
-            ])
+            ]))
             ->assertSessionHasErrors(['execution_date' => 'La fecha de la actividad es obligatoria']);
     }
 
     /** @test */
     function the_execution_date_must_have_a_valid_format() {
         $this->from('/family-authorization')
-            ->post('/family-authorization', [
-                'execution_date' => 'fecha-inválida',
-            ])
+            ->post('/family-authorization', $this->getValidData([
+                'execution_date' => 'invalid-date',
+            ]))
             ->assertSessionHasErrors(['execution_date' => 'La fecha de la actividad debe tener un formato válido']);
     }
 
     /** @test */
     function the_departure_time_is_required() {
         $this->from('/family-authorization')
-            ->post('/family-authorization', [
+            ->post('/family-authorization', $this->getValidData([
                 'departure_time' => null,
-            ])
+            ]))
             ->assertSessionHasErrors(['departure_time' => 'La hora de salida es obligatoria']);
     }
 
     /** @test */
     function the_departure_time_must_have_a_valid_format() {
         $this->from('/family-authorization')
-            ->post('/family-authorization', [
-                'departure_time' => 'hora-inválida',
-            ])
+            ->post('/family-authorization', $this->getValidData([
+                'departure_time' => 'invalid-time',
+            ]))
             ->assertSessionHasErrors(['departure_time' => 'La hora debe tener un formato válido']);
     }
 
     /** @test */
     function the_goals_is_required() {
         $this->from('/family-authorization')
-            ->post('/family-authorization', [
+            ->post('/family-authorization', $this->getValidData([
                 'goals' => null,
-            ])
+            ]))
             ->assertSessionHasErrors(['goals' => 'Los objetivos son obligatorios']);
     }
 
     /** @test */
     function the_deadline_is_required() {
         $this->from('/family-authorization')
-            ->post('/family-authorization', [
+            ->post('/family-authorization', $this->getValidData([
                 'deadline' => null,
-            ])
+            ]))
             ->assertSessionHasErrors(['deadline' => 'La fecha de entrega de la hoja es obligatoria']);
     }
 
     /** @test */
     function the_deadline_must_have_a_valid_format() {
         $this->from('/family-authorization')
-            ->post('/family-authorization', [
+            ->post('/family-authorization', $this->getValidData([
                 'deadline' => 'fecha-invalida',
-            ])
+            ]))
             ->assertSessionHasErrors(['deadline' => 'La fecha de entrega de la hoja debe tener un formato válido']);
     }
 
     /** @test */
     function the_parents_is_required() {
         $this->from('/family-authorization')
-            ->post('/family-authorization', [
+            ->post('/family-authorization', $this->getValidData([
                 'parents' => null,
-            ])
+            ]))
             ->assertSessionHasErrors(['parents' => 'El nombre del padre/madre/tutor es obligatorio']);
     }
 
     /** @test */
     function the_student_is_required() {
         $this->from('/family-authorization')
-            ->post('/family-authorization', [
+            ->post('/family-authorization', $this->getValidData([
                 'student' => null,
-            ])
+            ]))
             ->assertSessionHasErrors(['student' => 'El nombre del alumno es obligatorio']);
     }
 
@@ -197,27 +212,27 @@ class FamilyAuthorizationTest extends TestCase
     /** @test */
     function the_authorization_is_required() {
         $this->from('/family-authorization')
-            ->post('/family-authorization', [
+            ->post('/family-authorization', $this->getValidData([
                 'authorization' => null,
-            ])
+            ]))
             ->assertSessionHasErrors(['authorization' => 'La autorización es obligatoria']);
     }
 
     /** @test */
     function the_dni_is_required() {
         $this->from('/family-authorization')
-            ->post('/family-authorization', [
+            ->post('/family-authorization', $this->getValidData([
                 'dni' => null,
-            ])
+            ]))
             ->assertSessionHasErrors(['dni' => 'El DNI del padre/madre/tutor es obligatorio']);
     }
 
     /** @test */
     function the_dni_must_have_a_valid_format() {
         $this->from('/family-authorization')
-            ->post('/family-authorization', [
+            ->post('/family-authorization', $this->getValidData([
                 'dni' => '111A',
-            ])
+            ]))
             ->assertSessionHasErrors(['dni' => 'El DNI debe tener un formato válido']);
     }
 }
