a7bc996317e1e636b41bc299467d086d3c5fdb92
FuenSansano <fuensanosano@gmail.com>
2023-06-03 15:18:40 +0200

    REFACTOR: implement testHelper and made test more clear in ExtracurricularActivityTest class

--- tests/Feature/ExtracurricularActivityTest.php
+++ tests/Feature/ExtracurricularActivityTest.php
@@ -3,9 +3,27 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Tests\TestHelpers;
 
 class ExtracurricularActivityTest extends TestCase
 {
+    use TestHelpers;
+
+    protected array $defaultData = [
+        'activity_name' => 'a random activity name',
+        'activity_place' => 'a random activity place',
+        'activity_module' => 'a random activity module',
+        'activity_departament' => 'a random activity departament',
+        'teachers' => 'a random teachers',
+        'student_groups' => 'a random student groups',
+        'date' => '2000-01-01',
+        'departure_time' => '12:45',
+        'arrive_time' => '12:45',
+        'activity_price' => '12.50',
+        'transport' => 'a random transport',
+        'activity_responsible_teacher' => 'a random activity responsible teacher',
+    ];
+
     /** @test */
     function the_name_activity_field_is_in_the_extraescolar_activity_form_page()
     {
@@ -66,9 +84,9 @@ class ExtracurricularActivityTest extends TestCase
     function given_the_date_field_when_do_not_a_date_format_then_show_its_error()
     {
          $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity', $this->getValidData([
                 'date' => 'aszdfgyhujiklñ'
-            ])
+            ]))
             ->assertSessionHasErrors(['date' => 'El campo fecha de la actividad debe ser en formato día/mes/año']);
     }
 
@@ -124,9 +142,9 @@ class ExtracurricularActivityTest extends TestCase
     public function given_the_name_activity_field_is_empty_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'activity_name' => ''
-            ])
+            ]))
             ->assertSessionHasErrors(['activity_name' => 'El nombre de la actividad es obligatorio']);
     }
 
@@ -134,20 +152,19 @@ class ExtracurricularActivityTest extends TestCase
     public function given_the_activity_place_field_is_empty_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'activity_place' => ''
-            ])
+            ]))
             ->assertSessionHasErrors(['activity_place' => 'El lugar de la actividad debe ser obligatorio']);
     }
-
 
     /** @test */
     public function given_the_activity_module_field_is_empty_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'activity_module' => ''
-            ])
+            ]))
             ->assertSessionHasErrors(['activity_module' => 'El modulo al que pertenece la actividad debe ser obligatorio']);
     }
 
@@ -155,9 +172,9 @@ class ExtracurricularActivityTest extends TestCase
     public function given_the_activity_departament_field_is_empty_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'activity_departament' => ''
-            ])
+            ]))
             ->assertSessionHasErrors(['activity_departament' => 'El departamento al que pertenece la actividad debe ser obligatorio']);
     }
 
@@ -165,9 +182,9 @@ class ExtracurricularActivityTest extends TestCase
     public function given_the_teachers_field_is_empty_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'teachers' => ''
-            ])
+            ]))
             ->assertSessionHasErrors(['teachers' => 'El campo profesores que van a asistir a la actividad debe ser obligatorio']);
     }
 
@@ -175,9 +192,9 @@ class ExtracurricularActivityTest extends TestCase
     public function given_student_groups_field_is_empty_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'student_groups' => ''
-            ])
+            ]))
             ->assertSessionHasErrors(['student_groups' => 'El campo grupo de estudiantes debe ser obligatorio']);
     }
 
@@ -185,9 +202,9 @@ class ExtracurricularActivityTest extends TestCase
     public function given_date_field_is_empty_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'date' => ''
-            ])
+            ]))
             ->assertSessionHasErrors(['date' => 'El campo fecha de la actividad debe ser obligatorio']);
     }
 
@@ -195,9 +212,9 @@ class ExtracurricularActivityTest extends TestCase
     public function given_departure_time_field_is_empty_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'departure_time' => ''
-            ])
+            ]))
             ->assertSessionHasErrors(['departure_time' => 'El campo hora de salida debe ser obligatorio']);
     }
 
@@ -205,9 +222,9 @@ class ExtracurricularActivityTest extends TestCase
     public function given_departure_time_field_when_do_not_have_a_date_format_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'departure_time' => 'asdfghjkl'
-            ])
+            ]))
             ->assertSessionHasErrors(['departure_time' => 'El campo hora de salida debe de ser en formato hh:mm']);
     }
 
@@ -215,9 +232,9 @@ class ExtracurricularActivityTest extends TestCase
     public function given_arrive_time_field_is_empty_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'arrive_time' => ''
-            ])
+            ]))
             ->assertSessionHasErrors(['arrive_time' => 'El campo hora de llegada debe ser obligatorio']);
     }
 
@@ -225,9 +242,9 @@ class ExtracurricularActivityTest extends TestCase
     public function given_arrive_time_field_when_do_not_have_a_date_format_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'arrive_time' => 'asdfghjkl'
-            ])
+            ]))
             ->assertSessionHasErrors(['arrive_time' => 'El campo hora de llegada debe de ser en formato hh:mm']);
     }
 
@@ -235,9 +252,9 @@ class ExtracurricularActivityTest extends TestCase
     public function given_activity_price_field_is_empty_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'activity_price' => ''
-            ])
+            ]))
             ->assertSessionHasErrors(['activity_price' => 'El campo precio de la actividad debe ser obligatorio']);
     }
 
@@ -245,9 +262,9 @@ class ExtracurricularActivityTest extends TestCase
     public function given_activity_price_field_when_it_is_different_than_numberic_value_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'activity_price' => 'fcghvjbklm'
-            ])
+            ]))
             ->assertSessionHasErrors(['activity_price' => 'El campo precio debe de ser numérico y positivo']);
     }
 
@@ -255,9 +272,9 @@ class ExtracurricularActivityTest extends TestCase
     public function given_transport_field_is_empty_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'transport' => ''
-            ])
+            ]))
             ->assertSessionHasErrors(['transport' => 'El campo transporte debe ser obligatorio']);
     }
 
@@ -265,10 +282,9 @@ class ExtracurricularActivityTest extends TestCase
     public function given_activity_responsible_teacher_field_is_empty_then_show_its_error()
     {
         $this->from('/extracurricular-activity')
-            ->post('/extracurricular-activity', [
+            ->post('/extracurricular-activity',  $this->getValidData([
                 'activity_responsible_teacher' => ''
-            ])
+            ]))
             ->assertSessionHasErrors(['activity_responsible_teacher' => 'El campo responsable de la actividad debe ser obligatorio']);
     }
-
 }
