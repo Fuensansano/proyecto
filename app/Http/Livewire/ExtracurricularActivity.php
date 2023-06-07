21b157e4d0040082230bd14785ec3f1376edc6a3
tomasrl18 <tomaslopezraigal@gmail.com>
2023-06-07 22:12:17 +0200

    Prueba

--- app/Http/Livewire/ExtracurricularActivity.php
+++ app/Http/Livewire/ExtracurricularActivity.php
@@ -7,8 +7,6 @@ use Dompdf\Dompdf;
 use Dompdf\Options;
 use Livewire\Component;
 
-
-
 class ExtracurricularActivity extends Component
 {
     public function render()
@@ -42,8 +40,6 @@ class ExtracurricularActivity extends Component
         $dompdf->loadHtml($html);
         $dompdf->setPaper('A4', 'portrait');
         $dompdf->render();
-
-        \Illuminate\Http\Response::header('Content-type', 'application/pdf');
 
         return $dompdf->stream('ejemplo.pdf', array('Attachment' => 0));
     }
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,7 @@
+<header>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+</header>
+
 <div>
     <img src="{{ public_path('images/cabecerav6.png') }}" alt="cabecera.png">
 </div>
