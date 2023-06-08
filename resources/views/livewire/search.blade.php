64f0c03b5e54560750fd812c0cd31022e65e6160
FuenSansano <fuensanosano@gmail.com>
2023-06-08 21:44:18 +0200

    proof deploy

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid search-container">
-    <h1 class="title-principle-page">Encuentre el formulario que desee</h1>
+    <h1 class="title-principle-page">Buscador de formularios</h1>
     <select onchange="location = this.value;" name="selectedOption" class="form-select w-100 p-3" aria-label="formulario a buscar">
         @foreach(config('typeOfForms') as $key => $form)
             <option value="{{ $key }}">
