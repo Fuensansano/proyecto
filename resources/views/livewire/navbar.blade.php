8f262d35342562e78731815fa84c3cf7ab591613
FuenSansano <fuensanosano@gmail.com>
2023-06-13 20:37:45 +0200

    improved responsive mobile navbar

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,9 +1,13 @@
 <nav class="navbar navbar-expand-lg bg-primary">
     <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="/">
+            <img src="/images/ies-ingeniero-de-la-cierva-logo.webp" alt="Bootstrap" width="150" height="24">
+        </a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <a class="navbar-brand" href="/">
-                <img src="/images/ies-ingeniero-de-la-cierva-logo.webp" alt="Bootstrap" width="150" height="24">
-            </a>
+
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item dropdown">
                     <a class="nav-link text-light dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
